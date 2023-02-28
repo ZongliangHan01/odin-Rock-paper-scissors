2a49a978a3fa51121e9b9e486167b7246054b00b
zongliang han <zongliangh@student.unimelb.edu.au>
2023-02-28 22:46:41 +1100

    add simple UI to the game

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+
+const box = document.querySelector(".result");
+const result = document.createElement("div");
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+//buttons response
+rock.addEventListener("click", playRound);
+
+paper.addEventListener("click", playRound);
+
+scissor.addEventListener("click", playRound);
+
+//get the random choice of computer
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random()*100) + 1;
     if (randomNumber>66) {
@@ -5,43 +23,91 @@ function getComputerChoice() {
     } else if (randomNumber>33) {
         return "Paper"
     } else {
-        return "Scissors"
+        return "Scissor"
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.slice(0,1).toUpperCase()+playerSelection.slice(1).toLowerCase();
-    if ((playerSelection=="Rock" && computerSelection=="Scissors")
+//play one round when player click the button
+function playRound(e) {
+    let playerSelection = e.target.value 
+    computerSelection = getComputerChoice();
+    
+    //when player win
+    if ((playerSelection=="Rock" && computerSelection=="Scissor")
     || (playerSelection=="Paper" && computerSelection=="Rock")
-    || (playerSelection=="Scissors" && computerSelection=="Paper")) {
-        console.log (`You Win! ${playerSelection} beats ${computerSelection}!`);
-        return "player";
+    || (playerSelection=="Scissor" && computerSelection=="Paper")) {
+        updateMessage("player", playerSelection, computerSelection);
+    
+    //when its a tie game
     }else if (playerSelection==computerSelection) {
-        console.log (`It's a tie game! Both chose the ${playerSelection}!`);
-        return "tie";
+        updateMessage("tie", playerSelection, computerSelection);
+    
+    //when computer win 
     } else {
-        console.log (`You Lose! ${computerSelection} beats ${playerSelection}!`);
-        return "computer";
+        updateMessage("computer", playerSelection, computerSelection);
     }
+    
+    //when any win, display the winner info and ask player to reload game
+    if (scoreComputer==5 || scorePlayer==5) {
+        endGame();
+    }
+    
+    
 }
 
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i=0; i<5; i++) {
-        const playerSelection = prompt("What is your choice for this round");
-        const computerSelection = getComputerChoice();
-        let result = (playRound(playerSelection, computerSelection));
-        if (result == "player") {
-            playerScore++;
-        } else if (result == "computer") {
-            computerScore++;
-        }
+//display the winner info
+function endGame() {
+    //when computer win
+    if (scoreComputer==5)  {
+        result.textContent = "Computer win the game!";
+        
+    //when player win
+    } else if (scorePlayer==5) {
+        result.textContent = "You win the game!";    
     }
-    if (playerScore>computerScore) {
-        console.log("You are the winner!")
-    } else if (playerScore<computerScore) {
-        console.log("Computer is the winner!")
-    }
+    box.appendChild(result);
+    
+    //get rid of choice button
+    const diss = document.querySelectorAll(".disappear");
+    diss.forEach(dis => {
+        dis.remove();
+    });
+    
+    //create a reload button
+    const reloadBtn = document.createElement("button");
+    reloadBtn.textContent = "Play Again";
+    reloadBtn.addEventListener("click", reloadGame);
+    result.appendChild(reloadBtn);
 }
+
+//update info showing on the screen base on the winner
+function updateMessage(winner, playerSelection, computerSelection){
+    //prepare the text and update scores
+    if (winner === "computer") {
+        result.textContent = `You Lose this Round! ${computerSelection} beats ${playerSelection}!`;
+        ++scoreComputer;
+        
+    } else if (winner === "player") {
+        result.textContent = `You Win this Round! ${playerSelection} beats ${computerSelection}!`
+        ++scorePlayer;
+        
+    } else if (winner === "tie") {
+        result.textContent = `It's a tie game! Both chose the ${playerSelection}!`;
+    }
+
+    //add text and score to DOM tree
+    let score = document.querySelector(".scoreContent");
+    let scoreContent = document.createElement("div");
+    
+    scoreContent.textContent = `Player's score: ${scorePlayer}       Computer's score: ${scoreComputer}`;
+    scoreContent.classList.add("scoreContent");
+    score.replaceWith(scoreContent);
+    box.appendChild(result);
+}
+
+//reload game
+function reloadGame() {
+    window.location.reload();
+}
+
